83f9948036e0ae52e0cc3a4b01f803e8715deeab
shadowray1999 <slava.pavlenko2010@yandex.com>
2019-11-17 18:13:07 +0400

    Отрисовка фрактала (кривая Коха)

---
+++ Fractal/Fractal как надо/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Fractal_как_надо
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.draw_btn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // draw_btn
+            // 
+            this.draw_btn.Location = new System.Drawing.Point(12, 37);
+            this.draw_btn.Name = "draw_btn";
+            this.draw_btn.Size = new System.Drawing.Size(75, 23);
+            this.draw_btn.TabIndex = 1;
+            this.draw_btn.Text = "Draw";
+            this.draw_btn.UseVisualStyleBackColor = true;
+            this.draw_btn.Click += new System.EventHandler(this.Draw_btn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(149, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Степень фрактализации";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.draw_btn);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button draw_btn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Fractal/Fractal как надо/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fractal_как_надо
+{
+    public partial class Form1 : Form
+    {
+        string strstart = "F";
+        string ruleF = "F-F++F-F";
+        Graphics g;
+        Pen pen;
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+        }
+
+        private void Draw_btn_Click(object sender, EventArgs e)
+        {
+            g.Clear(pictureBox1.BackColor);
+            pen = new Pen(Color.Black);
+            int fractalizationDegree = 1;
+            try
+            {
+                fractalizationDegree = int.Parse(textBox1.Text);
+            }
+            catch { }
+            string str = strstart;
+            for (int i = 0; i < fractalizationDegree; i++)
+                str = str.Replace("F", ruleF);
+            double alpha = 0;
+            int step = 10;
+            int stepX = step;
+            int stepY = step;
+            int x = 10;
+            int y = pictureBox1.Size.Height - 10;
+            double c1 = 0.5;
+            double c2 = 0.86;
+            for (int i = 0; i < str.Length; i++)
+            {
+                switch (str[i])
+                {
+                    case 'F':
+                        switch (alpha)
+                        {
+                            case 0:
+                                stepX = step;
+                                stepY = 0;
+                                break;
+                            case 1:
+                                stepX = (int)(step * c1);
+                                stepY = (int)(step * c2);
+                                break;
+                            case 2:
+                                stepX = -(int)(step * c1);
+                                stepY = (int)(step * c2);
+                                break;
+                            case 3:
+                                stepX = -step;
+                                stepY = 0;
+                                break;
+                            case 4:
+                                stepX = -(int)(step * c1);
+                                stepY = -(int)(step * c2);
+                                break;
+                            case 5:
+                                stepX = (int)(step * c1);
+                                stepY = -(int)(step * c2);
+                                break;
+                        }
+                        g.DrawLine(pen, x, y, x + stepX, y + stepY);
+                        x += stepX;
+                        y += stepY;
+                        break;
+                    case '-':
+                        alpha -= 1;
+                        if (alpha < 0) alpha += 6;
+                        break;
+                    case '+':
+                        alpha += 1;
+                        if (alpha > 5) alpha -= 6;
+                        break;
+                }
+            }
+        }
+    }
+}
